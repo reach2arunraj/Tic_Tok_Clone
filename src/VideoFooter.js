6d4bbf97d6916c8fa8cededc9c3c4278ac6b8afb
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-09-04 18:01:31 +0530

    footer component styling

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,10 +1,32 @@
 import React from 'react'
 import "./VideoFooter.css"
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import Ticker from "react-ticker";
 
 function VideoFooter() {
     return (
         <div className="videoFooter">
-            
+            <div className="videoFooter__text">
+                <h3>@arunsmith60</h3>
+                <p>This is my description</p>
+                <div className="videoFooter__ticker">
+                    <MusicNoteIcon
+                    className="videoFooter__icon" />
+
+                    <Ticker mode="smooth">
+                    {({index}) =>(
+                        <>
+                            <p>I am a Song</p>
+                        </>
+                    )}
+                    </Ticker>
+                </div>
+            </div>
+            <img
+                className="videoFooter__record"
+                src="https://static.thenounproject.com/png/934821-200.png"
+                alt=""
+            />
         </div>
     )
 }
