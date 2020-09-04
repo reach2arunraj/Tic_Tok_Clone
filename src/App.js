42acc4e8d292ab189a1a933c189463792c786dd3
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-09-04 19:31:09 +0530

    passing props from App component to child components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ function App() {
   return (
     <div className="app">
       <div className="app_videos">
-        <Video />
+        <Video 
+        url={"https://v16m.tiktokcdn.com/96e923cd83f59981d2a6dbda7357d02a/5f524bb3/video/tos/useast2a/tos-useast2a-ve-0068c002/be73a082094e47768115c871bfdb865b/?a=1233&br=1856&bt=928&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20200904081353010190218201060009A0&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2xkbmZwZDp1czMzOjczM0ApZTc3Zzc5OzxlNzc1Ozk0M2c1MWMtX2cyYGhfLS00MTZzc19eM142MjE2XmA2MGAxXy06Yw%3D%3D&vl=&vr="}
+        channel="arunsmith60"
+        description="Wow it is Clone"
+        song="I am song from Tamil"
+        likes={111}
+        messages={222}
+        messages={333}
+        />
         <Video />
       </div>
     </div>
--- src/Video.js
+++ src/Video.js
@@ -3,7 +3,7 @@ import "./Video.css"
 import VideoFooter from './VideoFooter';
 import VideoSidebar from "./VideoSidebar"
 
-function Video() {
+function Video({url, channel, description, song, likes, messages, shares}) {
     const [playing, setPlaying] = useState(false)
     const videoRef = useRef(null);
 
@@ -24,10 +24,18 @@ function Video() {
                 loop
                 ref={videoRef}
                 onClick={handleVideoPress}
-                src="https://v16m.tiktokcdn.com/96e923cd83f59981d2a6dbda7357d02a/5f524bb3/video/tos/useast2a/tos-useast2a-ve-0068c002/be73a082094e47768115c871bfdb865b/?a=1233&br=1856&bt=928&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20200904081353010190218201060009A0&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M2xkbmZwZDp1czMzOjczM0ApZTc3Zzc5OzxlNzc1Ozk0M2c1MWMtX2cyYGhfLS00MTZzc19eM142MjE2XmA2MGAxXy06Yw%3D%3D&vl=&vr="
+                src={url}
             />
-            <VideoFooter channel="arunsmith60" description="Wow its a clone" song={"Bingo"} />
-            <VideoSidebar likes={111} messages={222} shares={333} />
+            <VideoFooter 
+            channel={channel}
+            description={description}
+            song={song} />
+
+            <VideoSidebar 
+            likes={likes}
+            messages={messages}
+            shares={shares} />
+
         </div>
     )
 }
